5eac1383cd091728e39a4c24f428f5535519eb55
weishi3 <weishi3@illinois.edu>
2016-03-09 19:40:16 +0000

    ini

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,23 @@
-var app = angular.module('mp3',['ngRoute']);
+var app = angular.module('mp3',[  'ngRoute','Controllers']);
 
-app.config(function ($routeProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
 
-})
+    $routeProvider
+        .when('/list', {
+            templateUrl : 'partials/list.html',
+            controller: 'listController'
+        })
+        .when('/details/:rank', {
+            templateUrl : 'partials/details.html',
+            controller: 'detailsController'
+        })
+        .when('/gallery', {
+            templateUrl : 'partials/gallery.html',
+            controller: 'galleryController'
+        })
+        .otherwise({
+            redirectTo: '/list'
+        });
+
+
+}]);
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,47 @@
 /* Sample Controller */
+var Controllers = angular.module('Controllers', []);
+
+Controllers.controller('listController', ['$scope','$http', function($scope,$http) {
+    $http.get('data/imdb250.json')
+        .success(function(data){
+            $scope.movies = data;
+            console.log(data);
+        })
+        .error(function (err){
+            console.log(err);
+        });
+
+    $scope.order='rank';
+    //set the default order
+
+
+}]);
+
+
+Controllers.controller('detailsController', ['$scope','$routeParams', '$http',function($scope,$routeParams,$http) {
+    $http.get('data/imdb250.json')
+        .success(function(data){
+            $scope.movies = data;
+            $scope.movie=data[($routeParams.rank)-1];
+            console.log(data);
+        })
+        .error(function (err){
+            console.log(err);
+        });
+    $scope.order='rank';
+
+
+}]);
+
+Controllers.controller('galleryController',['$scope','$http',function($scope,$http){
+    $http.get('data/imdb250.json')
+        .success(function(data){
+            $scope.movies = data;
+            console.log(data);
+        })
+        .error(function (err){
+            console.log(err);
+        });
+
+
+}]);
